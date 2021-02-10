257e04d55b810183642653cd091992200eabf8a7
marina01p <56072634+marina01p@users.noreply.github.com>
2021-02-10 14:24:02 +0200

    lab1 init

---
+++ ptrlab1/lib/ptrlab1.ex
@@ -0,0 +1,18 @@
+defmodule Ptrlab1 do
+  @moduledoc """
+  Documentation for `Ptrlab1`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ptrlab1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ ptrlab1/test/ptrlab1_test.exs
@@ -0,0 +1,8 @@
+defmodule Ptrlab1Test do
+  use ExUnit.Case
+  doctest Ptrlab1
+
+  test "greets the world" do
+    assert Ptrlab1.hello() == :world
+  end
+end
